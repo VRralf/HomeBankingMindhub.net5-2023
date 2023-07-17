909d778b7b3423a2d0742d33d643916341ab645c
Eduardo Mendoza <vrralf@gmail.com>
2023-07-17 03:18:50 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMindhub/Models/DbInicializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeBankingMindhub.Models
+{
+    public class DbInicializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if(context.Clients.Any()) { return ; }
+
+            var Clients = new Client[]
+            {
+                new Client{FirstName="Eduardo",LastName="Mendoza",Email="vrralf@gmail.com",Password="123456" },
+                new Client{FirstName="Pepito",LastName="Perez",Email="mail@mail.com",Password="123456"}
+            };
+
+            foreach (var client in Clients)
+            {
+                context.Clients.Add(client);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
